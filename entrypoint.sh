9d9e6e353ea5547433e8f49352c4eab528a3045f
okuzawats <okuzawats@gmail.com>
2022-08-31 11:20:56 +0900

    implement messaging action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+# 引数前後の""を除去する
+MESSAGE=`echo $3 | sed 's/"//g'`
+
+# Chatwork APIを用いてメッセージを投稿する。
+# $1: APIキー
+# $2: ルームID
+# $3: メッセージ
+curl \
+    -X POST \
+    -H "X-ChatWorkToken: $1" \
+    -d "body=$MESSAGE" \
+    "https://api.chatwork.com/v2/rooms/$2/messages"
