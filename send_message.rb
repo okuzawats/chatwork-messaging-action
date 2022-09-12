dc1bbe9d5bd7da5abc3d7bd257747b7e6ab94e92
okuzawats <okuzawats@gmail.com>
2022-09-12 22:42:56 +0900

    add title to action

--- send_message.rb
+++ send_message.rb
@@ -1,12 +1,17 @@
 require 'net/http'
 
-params = { token: ARGV[0], room_id: ARGV[1], message: ARGV[2][1, ARGV[2].length - 2] }
+params = { 
+  token: ARGV[0],
+  room_id: ARGV[1],
+  title: ARGV[2][1, ARGV[2].length - 2],
+  message: ARGV[3][1, ARGV[3].length - 2]
+}
 
 uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/messages")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 
-body = "body=[info][title]#{params[:message]}[/title]hello this is me.[/info]"
+body = "body=[info][title]#{params[:title]}[/title]#{params[:message]}[/info]"
 headers = { "X-ChatWorkToken" => "#{params[:token]}" }
 
 response = http.post(uri.path, body, headers)
