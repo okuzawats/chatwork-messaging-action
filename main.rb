8cccdd01bd5399bb2a51650584d1c3761fc7bccf
shikajiro <shikajiro@gmail.com>
2023-10-20 16:18:38 +0900

    タスクを作成するAPI追加

--- main.rb
+++ main.rb
@@ -3,23 +3,31 @@ require 'net/http'
 params = { 
   token: ENV['API_TOKEN'],
   room_id: ENV['ROOM_ID'],
-  message: ENV['MESSAGE'].delete_prefix('"').delete_suffix('"')
+  message: ENV['MESSAGE'].delete_prefix('"').delete_suffix('"'),
+  task_user_id: ENV['TASK_USER_ID'],
 }
 
 if params[:message].empty?
   raise StandardError.new("empty message is not allowed.")
 end
 
-uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/messages")
+base_uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/")
+body = "body=#{params[:message]}"
+if params[:task_user_id]
+  uri = base_uri + "tasks"
+  body += "&to_ids=#{params[:task_user_id]}"
+else
+  uri = base_uri + "messages"
+end
+
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 
-body = "body=#{params[:message]}"
 headers = { "X-ChatWorkToken" => "#{params[:token]}" }
 
 response = http.post(uri.path, body, headers)
 
-if response.code == '200' && response.body.match(/^{"message_id":"[0-9]+"}/)
+if response.code == '200'
   puts response.body
 else
   raise StandardError.new("action failed! #{response.body}")
