1c0b6812d9785a07350009dd2d0c79f715bc31a4
okuzawats <okuzawats@gmail.com>
2024-08-08 13:30:01 +0900

    remove deprecated task feature (#53)
    * remove task feature
    
    * use single quote instead of double quote
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * provide exception class nad message as arguments to raise
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * use to_s
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- main.rb
+++ main.rb
@@ -4,51 +4,19 @@ params = {
   token: ENV['API_TOKEN'],
   room_id: ENV['ROOM_ID'],
   message: ENV['MESSAGE'].delete_prefix('"').delete_suffix('"'),
-  message_type: ENV['MESSAGE_TYPE'],
-  user_ids_to_assign_task: ENV['USER_IDS_TO_ASSIGN_TASK']
 }
 
-# message type must be message or task.
-type = params[:message_type]
-valid_types = ["message", "task"]
-unless valid_types.include?(type)
-  raise StandardError.new("type should be message or task.")
-end
+uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/messages")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
 
-if params[:message].empty?
-  raise StandardError.new("empty message is not allowed.")
-end
+body = "body=#{params[:message]}"
+headers = { 'X-ChatWorkToken' => params[:token].to_s }
 
-if type == "message"
-  uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/messages")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
+response = http.post(uri.path, body, headers)
 
-  body = "body=#{params[:message]}"
-  headers = { "X-ChatWorkToken" => "#{params[:token]}" }
-
-  response = http.post(uri.path, body, headers)
-
-  if response.code == '200'
-    puts response.body
-  else
-    raise StandardError.new("action failed! #{response.body}")
-  end
-end
-
-if type == "task"
-  uri = URI.parse("https://api.chatwork.com/v2/rooms/#{params[:room_id]}/tasks")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-
-  body = "body=#{params[:message]}&to_ids=#{params[:user_ids_to_assign_task]}"
-  headers = { "X-ChatWorkToken" => "#{params[:token]}" }
-
-  response = http.post(uri.path, body, headers)
-
-  if response.code == '200'
-    puts response.body
-  else
-    raise StandardError.new("action failed! #{response.body}")
-  end
+if response.code == '200'
+  puts response.body
+else
+  raise StandardError, "action failed! #{response.body}"
 end
